1d90f4decb683ff9efcd84ee2938128e0e6325f8
Erasmo <erasmo.aln@gmail.com>
2021-04-04 12:57:39 -0300

    input app
    Takes values in python's inputs and add to the dataset.csv

---
+++ src/app.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from datetime import datetime as dt
+
+
+date_today = dt.today().strftime('%Y-%m-%d')
+
+begin = input("Start time (for example, 13:47): ")
+end = input("End time (for example, 14:37): ")
+platform = input("Platform (DSA, MIT OCW...): ")
+subject = input("Subject (Machine Learning, Probability...): ")
+section = input("Section (Chapter 3, Lecture 5...): ")
+total = int(input("Total time (in minutes): "))
+
+data_to_append = {
+    "Date": [date_today],
+    "Begin": [begin],
+    "End": [end],
+    "Platform": [platform],
+    "Subject": [subject],
+    "Section": [section],
+    "Total": [total]
+}
+
+data_to_append_dataframe = pd.DataFrame.from_dict(data=data_to_append)
+
+data_to_append_dataframe.to_csv(path_or_buf="../data/dataset.csv", sep=";", index=False, mode="a", header=False)
