3bd206d966b8eee3994374a66880f6ad68986fd7
Erasmo <erasmo.aln@gmail.com>
2021-04-04 12:56:24 -0300

    setup file to create an empty dataset

---
+++ setup/create_dataset.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+columns = {
+    "Date": pd.Series([], dtype="object"),
+    "Begin": pd.Series([], dtype="object"),
+    "End": pd.Series([], dtype="object"),
+    "Platform": pd.Series([], dtype="object"),
+    "Subject": pd.Series([], dtype="object"),
+    "Section": pd.Series([], dtype="object"),
+    "Total": pd.Series([], dtype="int32")
+}
+
+dataset = pd.DataFrame.from_dict(columns)
+
+dataset.to_csv("../data/dataset.csv", sep=";", index=False)
