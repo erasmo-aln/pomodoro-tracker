42be57c802f3c330416cad39a5bae07cce9f36b9
Erasmo <erasmo.aln@gmail.com>
2021-06-11 17:08:35 -0300

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,31 +3,31 @@ import pandas as pd
 import altair as alt
 from datetime import datetime as dt
 from src.utils import utils
-import os
 
-PATH_TO_FOLDER = 'data'
-PATH_TO_DATA = 'data/dataset.csv'
 
-try:
-    dataset = utils.read_dataset(PATH_TO_DATA)
+FOLDER = 'data'
+DATA = 'data.csv'
+PATH_TO_DATA = '/'.join([FOLDER, DATA])
 
-except FileNotFoundError:
-    if not os.path.exists(PATH_TO_FOLDER):
-        utils.create_data_folder(PATH_TO_FOLDER)
-    utils.create_dataset(PATH_TO_DATA)
-    dataset = utils.read_dataset(PATH_TO_DATA)
+
+dataset = utils.get_dataset(PATH_TO_DATA)
 
 st.title(body='Pomodoro Tracker')
 select_option = st.radio(label='Fill or View?', options=['Fill', 'View'])
+
 submitted = False
 
 if select_option == 'Fill':
-    form_platform = st.sidebar.radio(label='Platforms Available', options=list(dataset.Platform.unique()))
-    form_subject = st.sidebar.radio(label='Subjects Available', options=list(dataset.Subject.loc[dataset.Platform == form_platform].unique()))
-    form_section = st.sidebar.radio(label='Sections Available', options=list(dataset.Section.loc[(dataset.Platform == form_platform) & (dataset.Subject == form_subject)].unique()))
+    st.sidebar.title(body='Information Available')
+
+    form_platform = st.sidebar.radio(label='Platforms', options=list(dataset.Platform.unique()))
+    form_subject = st.sidebar.radio(label='Subjects', options=list(dataset.Subject.loc[dataset.Platform == form_platform].unique()))
+    form_section = st.sidebar.radio(label='Sections', options=list(dataset.Section.loc[(dataset.Platform == form_platform) & (dataset.Subject == form_subject)].unique()))
+
     with st.form('Data Form'):
-        date_today = dt.today().strftime('%Y-%m-%d')
         st.write("Fill out the information about your Pomodoro")
+
+        date_today = dt.today().strftime('%Y-%m-%d')
 
         # Start Time
         st.subheader('Start time')
@@ -52,7 +52,9 @@ if select_option == 'Fill':
     if submitted:
         start_time = f'{start_hour}:{start_min}'
         end_time = f'{end_hour}:{end_min}'
+
         total = (end_hour * 60 + end_min) - (start_hour * 60 + start_min)
+
         data_to_append = {
             "Date": [date_today],
             "Begin": [start_time],
@@ -62,13 +64,14 @@ if select_option == 'Fill':
             "Section": [section],
             "Total": [total]
         }
+
         data_to_append_dataframe = pd.DataFrame.from_dict(data=data_to_append)
-        data_to_append_dataframe.to_csv(path_or_buf="data/data.csv", sep=";", index=False, mode="a", header=False)
+        data_to_append_dataframe.to_csv(path_or_buf=PATH_TO_DATA, sep=";", index=False, mode="a", header=False)
 
         with st.beta_container():
             startcol, endcol = st.beta_columns(2)
-            startcol.write('The data was succesfully saved. To refresh the data, click the button at the right.')
-            endcol.button('Rerun')
+            startcol.write('The data was succesfully saved. To refresh the data, click the Refresh button.')
+            endcol.button('Refresh')
 
 else:
     st.sidebar.title(body='Visualization Options')
@@ -78,18 +81,17 @@ else:
     section = st.sidebar.radio(label='Choose the chart', options=['Overall', 'Sections'])
 
     data2show = dataset.loc[(dataset.Platform == platform) & (dataset.Subject == subject)]
+
     data2show_grouped = data2show.groupby('Date')['Total'].sum()
+
     data2show_grouped2 = data2show.groupby('Section')['Total'].sum()
 
-    chart_barh = alt.Chart(data2show_grouped2.reset_index()).mark_bar().encode(
-        x=alt.X('sum(Total):Q', axis=alt.Axis(title='Total (in minutes)')),
-        y=alt.Y('Section', axis=alt.Axis(title=''))
-    ).properties(
-        width=700,
-        height=300
-    ).configure_axis(
-        grid=False
-    )
+    chart_barh = alt.Chart(
+        data2show_grouped2.reset_index()
+        ).mark_bar().encode(
+            x=alt.X('sum(Total):Q', axis=alt.Axis(title='Total (in minutes)')),
+            y=alt.Y('Section', axis=alt.Axis(title=''))
+            ).properties(width=700, height=300).configure_axis(grid=False)
 
     if section == 'Sections':
         st.altair_chart(chart_barh)
